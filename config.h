dd5ce17cc6f2513d57be664ab5c9baa6dc8b4b71
Krystian Kowal <krystian@kowal.pro>
2019-05-28 09:55:29 +0200

    Change monocle icon

--- config.h
+++ config.h
@@ -46,9 +46,9 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "",      tile },    /* first entry is default */
+	{ "",     tile },    /* first entry is default */
 	{ "",      NULL },    /* no layout function means floating behavior */
-	{ "",      monocle },
+	{ "",      monocle },
 };
 
 /* key definitions */
